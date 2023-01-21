ef8f9a54264c7cb896ac349e75075d74883c8beb
arsyadbruh <marsyad@student.ittelkom-sby.ac.id>
2023-01-21 10:43:28 +0700

    add validation form

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def hello():
         driverexp = request.form["driverexp"]
         weather = request.form["weather"]
         
+        if (age == "0" or gender == "0" or driverexp == "0" or weather == "0"):
+            flash(validationMessage(age, gender, driverexp, weather), "danger")
+            return render_template("index.html")
+            
+        
         y_pred = Traffic.severityArray(age, gender, driverexp, weather)
         
         severityPrediction = model.predict(y_pred)
@@ -48,6 +53,19 @@ def loadmodel():
             0,0,0,0,0,1,
             0,1,0,0,0,0,0,0
             ]]))
+    
+def validationMessage(inputAge, inputGender, inputExp, inputWeather):
+    message = "Input Not Valid"
+    if (inputAge == "0"):
+        message = "Choose Driver Age"
+    elif (inputGender == "0"):
+        message = "Choose Driver Gender"
+    elif (inputExp == "0"):
+        message = "Choose Driver Experience"
+    elif (inputWeather == "0"):
+        message = "Choose Weather Condition"
+
+    return message
 
 if __name__ == '__main__':
     app.secret_key = 'uniks3cr3tk3y'
